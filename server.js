c6d4fb0835900a28030952459ff636cf56c416bb
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2014-08-22 12:47:36 -0500

    Add operations for sell , buy and balance .

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express')
 var  url = require("url");
 var app = express();
 var user = require('./users');
+var wallet = require('./wallet');
 app.use(express.json());
 app.use(express.urlencoded())
 
@@ -23,7 +24,7 @@ var allowCrossDomain = function(req, res, next) {
 
 app.use(allowCrossDomain);
 
-app.get('api/ping', function(req, res){
+app.get('/api/ping', function(req, res){
 	var body = 'pong';
 	res.setHeader('Content-Type', 'text/plain');
 	res.setHeader('Content-Length', Buffer.byteLength(body));
@@ -34,6 +35,11 @@ app.get('api/ping', function(req, res){
 app.get('/api/createsession',user.createsession);
 app.post('/api/login',user.login);
 app.post('/api/register', user.register);
+app.post('/api/buy', wallet.buy);
+app.post('/api/balance', wallet.balance);
+app.post('/api/transfer', wallet.transfer);
+app.post('/api/sell', wallet.sell);
+
 
 app.listen( process.env.PORT  || 3000);
 console.log('Listening on port 3000');
