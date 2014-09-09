625887b90217388ae4d0c283a0ca2b9a9868bccd
Alejandro Blanco <alexblanco94@gmail.com>
2014-09-09 16:59:07 -0500

    Error checker and additional info added to order recovery function

--- model/orderQueryBuilder.js
+++ model/orderQueryBuilder.js
@@ -4,7 +4,11 @@ exports.validateOrders = function(userID,callback){
 	console.log('Search user in mongoDB');
 	Order.find({ 'userId': userID }, '_id userId products.name products.quantity products.cost total date status', function (err, person) {
 		if (err) return handleError(err); 
-		console.log(person);
-		callback(null,person);
+		else if(!person)
+			callback("ERROR", { statusCode: 1 ,  additionalInfo: 'User is not yet registered' });
+		else{
+			var  response =   { statusCode: 0 ,  additionalInfo: person };
+			callback(null, response);
+		}
 	});
 };
