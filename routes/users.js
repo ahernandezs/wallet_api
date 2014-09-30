4873036924512c97cf2f70a8f37aecd5abccc959
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2014-09-30 13:21:04 -0500

    Delete json property for session .

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,10 @@ exports.login =  function(req, res){
   console.log('execute POST method login');
   console.log(req.body);
   sessionUser.loginFlow(req.body,function(err,result){
-      res.setHeader('X-AUTH-TOKEN', result.sessionid);
+      if(result.statusCode === 0){
+        res.setHeader('X-AUTH-TOKEN', result.sessionid);
+        delete result.sessionid;
+      }
       res.json(result);
   });
 };
