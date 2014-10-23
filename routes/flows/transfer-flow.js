2458e767134f7b3db2340bbd46ae9c5173094e76
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2014-10-23 11:39:25 -0500

    Add changes for mailing .

--- routes/flows/transfer-flow.js
+++ routes/flows/transfer-flow.js
@@ -206,6 +206,7 @@ exports.transferFunds = function(data, callback) {
             message.title = title;
             message.phoneID = payload.phoneID;
             message.date = dateTime;
+            message.message = payload.message;
             messageQuery.createMessage(forReceipt.user.data.phoneID,message, function(err, result) {
                 if (err) {
                     var response = { statusCode: 1, additionalInfo: result };
