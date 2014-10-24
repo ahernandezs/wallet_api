8d64661d187087b3e822c8af38d46617382a4573
Armando Máximo Hernández <ahernandezs@anzen.com.mx>
2014-10-24 17:45:49 -0500

    sent coupon flow

--- routes/flows/coupon-flow.js
+++ routes/flows/coupon-flow.js
@@ -5,37 +5,55 @@ var config = require('../../config.js');
 var crypto = require('crypto');
 var ReceiptQuery = require('../../model/queries/receipt-query');
 var PrizeQuery = require('../../model/queries/prize-query');
+var messageQuery = require('../../model/queries/message-query');
+var urbanService = require('../../services/urban-service');
 
 exports.setCoupon = function(payload,callback) {
+
+    var dateTime = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+    var prize = {};
 
 	async.waterfall([
 
 		function(callback){
-
-			//save message
-
-			callback(null);
-
-		},
-
-
-		function(callback){
-			
-			//send push
-
-			callback(null);
-
-		},
-
-		function(callback){
 			PrizeQuery.getPrize(payload.prizeID, function(err, result){
-				callback(null, result);
+				prize = result;
+				callback(null);
 			});
 		},
 
-		function(prize, callback){
+		function(callback){
 
-            console.log( 'Create Receipt Coupon' );
+            var message = {};
+            message.status = 'DELIVERED';
+            message.type = 'COUPON';
+            message.title = config.messages.coupon;
+            message.phoneID = payload.phoneID;
+            message.date = dateTime;
+            message.message = config.messages.coupon;
+            message.additionalInfo = prize;
+
+            messageQuery.createMessage(payload.phoneID, message, function(err, result) {
+                if (err) {
+                    var response = { statusCode: 1, additionalInfo: result };
+                    callback('ERROR', response);
+                } else {
+                    callback(null,payload);
+                }
+            });
+		},
+
+        function(message, callback) {
+			var extraData = { action: 1, additionalInfo: JSON.stringify(prize)};
+			message.extra = { extra:extraData};
+            message.message = config.messages.coupon;
+            urbanService.singlePush(message, function(err, result) {
+                callback(null);
+            });
+        },
+
+		function(callback){
+
             var receipt = {};
             receipt.emitter = payload.phoneID;
             receipt.receiver = payload.phoneID;
@@ -43,14 +61,15 @@ exports.setCoupon = function(payload,callback) {
             receipt.message = config.messages.coupon;
             receipt.additionalInfo = prize;
             receipt.title = config.messages.coupon;
-            receipt.date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+            receipt.date = dateTime;
             receipt.type = 'COUPON';
             receipt.status = 'DELIVERED';
             ReceiptQuery.createReceipt(receipt, function(err, result) {
                 if (err)
                     callback('ERROR', err);
                 else
-                    callback(null, result);
+					var response = { statusCode: 0, additionalInfo: 'The coupon sent was successful' };
+                    callback(null, response);
             });
 
 		},
