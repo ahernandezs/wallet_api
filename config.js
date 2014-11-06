ff540001536c1071d3107dd2ff65053fe1d80b79
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2014-11-05 22:00:10 -0600

    FIX -> Remove url facebook .

--- config.js
+++ config.js
@@ -49,7 +49,7 @@ config.messages.facebook =	{
 								name:'AmdocsCafe',
 								caption:'I just bought a coffee',
 								description:'Just dowloaded my Amdocs Wallet at Mobile Money Global! Looking forward to the delicous coffee at the fully cashless Amdocs Café, now open at Grand Wyndham, Istanbul',
-								link:'http://goo.gl/IRbh4z',
+								link:'',
 								picture: '',
 							};
 config.doxs = {};
