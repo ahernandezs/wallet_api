5f018e1217358018bd361d3ab741b89b88da289d
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2014-11-18 10:55:31 -0600

    Add field company for list users .

--- model/queries/merchant-query.js
+++ model/queries/merchant-query.js
@@ -81,12 +81,11 @@ exports.getMerchands = function(phoneID, callback){
         dateTime = new Date().getHours();
         var tmp = data[0].schedule.split('-');
 
-        var openTime = parseInt(tmp[0].replace(':00', '').replace(' ', ''));
-        var closeTime = parseInt(tmp[1].replace(':00', '').replace(' ', ''));
+
         var openTime = parseInt(tmp[0].replace('am', '').replace(' ', ''));
         var closeTime = parseInt(tmp[1].replace('pm', '').replace(' ', '')) + 12;
 
-        if(dateTime>=openTime && dateTime<=closeTime){
+        if(dateTime>=openTime && dateTime<closeTime){
             data[0].open = 1;
         }else{
             data[0].open = 0;
