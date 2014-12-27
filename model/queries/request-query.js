67e1a0502aa400f04610e37b3e937bbc84bfc407
Jesus Macedo <macedo.rjpablo@gmail.com>
2014-12-26 19:45:16 -0600

    Modified requestMoney and resolveRequest. Now getting the moneyRequest from mongo and updatinng it. There is still missing the transferFunds call and receipts creation.

--- model/queries/request-query.js
+++ model/queries/request-query.js
@@ -2,11 +2,33 @@ var RequestMoney = require('../requestMoney');
 var config = require('../../config.js');
 
 exports.createRequest = function(data, callback) {
+    console.log(data);
     var request = new RequestMoney(data);
     request.save(function(err, result) {
         if (err)
             callback('ERROR', 'The request could not be created.');
         else
             callback(null, result._id);
+    });
+};
+
+exports.getSingleRequest = function(requestID, callback) {
+    RequestMoney.findOne( { '_id' : requestID }, function(err, result) {
+       if (err)
+           callback('ERROR', 'The request was not found');
+        else
+            callback(null, result);
+    });
+};
+
+exports.updateRequest = function(data, callback) {
+    var conditions = { '_id' : data._id };
+    var request = { sender : data.sender, destinatary : data.destinatary, amount : data.amount, message : data.message,
+                    status : data.status, date : data.date };
+    RequestMoney.findOneAndUpdate( conditions, request, function(err, result) {
+       if (err)
+           callback('ERROR', 'There was an error updating the requestMoney.');
+        else
+            callback(null, result);
     });
 };
--- routes/flows/requestMoney-flow.js
+++ routes/flows/requestMoney-flow.js
@@ -34,44 +34,58 @@ exports.requestMoneyFlow = function(payload,callback) {
 
             });
 		},
-		function(senderName,senderAvatar,callback){
+        
+        function(senderName, senderAvatar, callback) {
+              console.log('Get receiver in db ' + requestMessage.destinatary);
+            Userquery.getName(requestMessage.destinatary,function(err,user){
+                if (err) {
+                    var response = { statusCode: 1, additionalInfo: err };
+                    callback('ERROR', response);
+                } else {
+                    console.log(config.S3.url);
+                    callback(null, user.name, senderName, senderAvatar);
+                }
+
+            });
+        },
+        
+        function (receiverName, senderName, senderAvatar, callback)
+        {
+            console.log('Save requestMoney in DB');
+            var requestMsg = 'You have sent a money transfer request to ' + receiverName;
+            var dateTime = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+            var data = { sender : payload.phoneID, destinatary : payload.destinatary, amount : payload.amount,
+                            message : requestMsg, status : config.requests.status.NEW , date : dateTime};
+            requestQuery.createRequest(data, function(err, result) {
+                if (err)
+                    callback('ERROR', { statusCode : 1, additionalInfo : 'The request could not be sent.' });
+                else
+                    callback(null, senderName, senderAvatar, result);
+            });
+        },
+        
+		function(senderName, senderAvatar, requestID, callback) {
             console.log('Save message in DB');
             var message = {};
             message.status = config.messages.status.NOTREAD;
             message.type = config.messages.type.REQUEST_MONEY;
-            var requestMsg = 'You have received a money transfer request from ' + senderName;
             message.title = 'You have received a money transfer request from ' + senderName;
-            message.phoneID = payload.destinitary;
+            message.phoneID = payload.destinatary;
             message.date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
             message.message = requestMessage.message;
-            message.additionalInfo = JSON.stringify({phoneID:payload.phoneID , name: senderName  , avatar :senderAvatar ,  amount : requestMessage.amount , message : requestMessage.message });
-            messageQuery.createMessage(requestMessage.phoneID,message, function(err, result) {
+            message.additionalInfo = JSON.stringify({phoneID:payload.phoneID, name: senderName, avatar :senderAvatar,  amount : requestMessage.amount, message : requestMessage.message, requestID : requestID });
+            messageQuery.createMessage(requestMessage.phoneID, message, function(err, result) {
                 if (err) {
                     var response = { statusCode: 1, additionalInfo: result };
                     callback('ERROR', response);
                 } else {
-                    payload.phoneID = payload.destinatary;
                     payload.message = message.title;
+                    payload.phoneID = payload.destinatary;
                     var extraData = {   action: 6, additionalInfo :  message.additionalInfo ,
                                     _id:result._id };
                     payload.extra = { extra:extraData};
-                    callback(null, payload, requestMsg);
+                    callback(null, payload);
                 }
-            });
-        },
-        
-        function (message, requestMsg, callback)
-        {
-            console.log('Save requestMoney in DB');
-            message.message = requestMsg;
-            var dateTime = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-            var data = { sender : payload.phoneID, destinatary : payload.destinitary, amount : payload.amount,
-                            message : requestMsg, status : config.requests.status.NEW , date : dateTime};
-            requestQuery.createRequest(data, function(err, result) {
-                if (err)
-                    callback('ERROR', { statusCode : 1, additionalInfo : 'The answer could not be sent.' });
-                else
-                    callback(null, message);
             });
         },
         
@@ -160,23 +174,100 @@ exports.requestMoneyConfirm = function(payload,callback) {
 
 exports.resolveRequestFlow = function(payload, callback) {
     console.log( payload );
-    // If NOT accepted
-    if( payload.answer === 'REJECTED' ) {
-        callback('STOP', 'The answer was sent correctly.');
-    } else {
+    var accepted = ( payload.answer === config.requests.status.ACCEPTED ) ? true : false;
         async.waterfall([
+           function(callback) {
+               console.log('Get sender in db ' + payload.phoneID);
+                Userquery.getName(payload.phoneID,function(err,user) {
+                    if (err) {
+                        var response = { statusCode: 1, additionalInfo: err };
+                        callback('ERROR', response);
+                    } else {
+                        console.log(config.S3.url);
+                        senderAvatar = config.S3.url + payload.phoneID +'.png';
+                        callback(null, user.name, senderAvatar);
+                    }
+
+                });
+           },
+            function(username, avatar, callback) {
+                if ( !accepted )
+                    callback(username, avatar, callback);
+                else {
+                    console.log( 'Getting requestMoney from Mongo' );
+                    requestQuery.getSingleRequest(payload.requestID, function(err, result) {
+                        if (err)
+                            callback('ERROR', 'There was an error sending the answer.');
+                        else {
+                            var request = result;
+                            request.status = payload.answer;
+                            request.date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+                            callback(null, request, username, avatar);
+                        }
+                    });
+                }
+            },
+            function(request, username, avatar, callback) {
+                console.log( 'Update requestMoney' );
+                console.log(request);
+                requestQuery.updateRequest(request, function(err, result) {
+                    console.log(result);
+                   if (err)
+                       callback('ERROR', 'There was an error sending the answer.' );
+                    else
+                        callback(null, username, avatar);
+                });
+            },
+            function(username, avatar, callback) {
+                if (!accepted)
+                    callback(null, username, avatar);
+                else {
+                    console.log( 'Making transferFunds' );   
+                }
+            },
+            function(username, avatar, callback) {
+                /*console.log('Save message in DB');
+                var message = {};
+                message.status = config.messages.status.NOTREAD;
+                message.type = config.messages.type.REQUEST_MONEY;
+                message.title = 'Your money transfer request ' + username + ' has been ' + payload.answer;
+                message.phoneID = payload.destinitary;
+                message.date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+                message.message = payload.message;
+                message.additionalInfo = JSON.stringify({ phoneID: payload.phoneID, name: username, avatar: avatar, message: payload.message });
+                messageQuery.createMessage(message.phoneID, message, function(err, result) {
+                    if (err) {
+                        var response = { statusCode: 1, additionalInfo: result };
+                        callback('ERROR', response);
+                    } else {
+                        payload.phoneID = payload.destinatary;
+                        payload.message = message.title;
+                        var extraData = {   action: 6, additionalInfo :  message.additionalInfo, _id:result._id };
+                        payload.extra = { extra:extraData};
+                        callback(null, payload);
+                    }
+                });*/
+            },
             function(callback) {
-                console.log('Getting receiver from message info');
-            }
-        ],
-             function (err, result) {
-                if (err) {
-                    console.log('Error  --->' + JSON.stringify(result));
-                    callback("Error! " + err, result);    
-                } else {
-                    callback(null,result);    
+                console.log( 'send push notification.' );
+               /*urbanService.singlePush(, function(err, result) {
+                    callback(null, { statusCode : 0, additionalInfo: 'The answer was sent correctly.' }); 
+               });*/
+            },
+            function(callback) {
+                if (!accepted)
+                    callback(null, { statusCode : 0, additionalInfo : 'The answer was sent corrrectly.' });
+                else {
+                    console.log( 'Getting balance' );
                 }
             }
-        );
-    }
+        ],
+        function (err, result) {
+            if (err) {
+                console.log('Error  --->' + JSON.stringify(result));
+                callback("Error! " + err, result);    
+            } else {
+                callback(null,result);    
+            }
+        });
 };
