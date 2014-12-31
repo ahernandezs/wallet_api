c29e02b0b2a480db44aef7f2fb1b786e0efa8b28
Jesus Macedo <macedo.rjpablo@gmail.com>
2014-12-31 12:33:48 -0600

    Restored additionalInfo for messages.

--- routes/flows/requestMoney-flow.js
+++ routes/flows/requestMoney-flow.js
@@ -170,8 +170,8 @@ exports.resolveRequestFlow = function(payload, header, callback) {
             function(balance, username, avatar, callback) {
                 var message = {};
                 message.title = 'Your money request to ' + username + ' was ' + payload.answer;
+                message.additionalInfo = JSON.stringify({ phoneID: payload.phoneID, name: username, avatar: avatar, message: payload.message });
                 var messageID;
-                
                 if(!accepted) {
                     console.log('Save message in DB');
                     message.status = config.messages.status.NOTREAD;
