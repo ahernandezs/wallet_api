c79bbab016096a541427264bd01ab0b91ce6c11d
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2015-03-24 20:19:11 -0600

    Add validations for missing parameters before invoke banamex services .

--- routes/payments-bmx.js
+++ routes/payments-bmx.js
@@ -9,46 +9,63 @@ exports.login =  function(req, res){
   var payload = req.body;
   console.log('Payload'+JSON.stringify(payload));
   console.log('URL base ' + url_base);
-  rest.post(url_base + '/login', {
-    data: { usuario : payload.user,
-            password : payload.password,
-            referencia : payload.reference,
-            concepto : payload.concept,
-            monto: payload.amount },
-      }).on('complete', function(data, response) {
-      if(data){
-        response = {statusCode:0, additionalInfo:data};
-        res.json(response);
-      }else{
-        response = {statusCode:1, additionalInfo:data};
-        res.json(response);
-      }
-  });
+  if(payload.user && payload.reference && payload.concept && payload.amount){
+    var response = {statusCode:1, additionalInfo:'Invalid parameters or missing parameters'};
+    res.json(response);
+  }
+  else{
+    rest.post(url_base + '/login', {
+      data: { usuario : payload.user,
+              password : payload.password,
+              referencia : payload.reference,
+              concepto : payload.concept,
+              monto: payload.amount },
+        }).on('complete', function(data, response) {
+        if(data){
+          response = {statusCode:0, additionalInfo:data};
+          res.json(response);
+        }else{
+          response = {statusCode:1, additionalInfo:data};
+          res.json(response);
+        }
+    });
+  }
 };
 
 exports.challenge =  function(req, res){
   console.log('execute POST MTS challenge ');
   var payload = req.body;
   console.log('Payload'+JSON.stringify(payload));
-  rest.post(url_base + '/challenge', {
-    data: { response: payload.response,
-            session_set: payload.session_set },
-      }).on('complete', function(data, response) {
-      if(data){
-        console.log(data);
-        response = {statusCode:0, additionalInfo:data};
-        res.json(response);
-      }else{
-        response = {statusCode:1, additionalInfo:data};
-        res.json(response);
-      }
-  });
+  if(payload.reference && payload.session_set){
+    var response = {statusCode:1, additionalInfo:'Invalid parameters or missing parameters'};
+    res.json(response);
+  }
+  else{
+    rest.post(url_base + '/challenge', {
+      data: { response: payload.response,
+              session_set: payload.session_set },
+        }).on('complete', function(data, response) {
+        if(data){
+          console.log(data);
+          response = {statusCode:0, additionalInfo:data};
+          res.json(response);
+        }else{
+          response = {statusCode:1, additionalInfo:data};
+          res.json(response);
+        }
+    });
+  }
 };
 
 exports.payment =  function(req, res){
   console.log('execute POST MTS payment ');
   var payload = req.body;
   console.log('Payload'+JSON.stringify(payload));
+  if(payload.cuenta_cargo && payload.session_set){
+    var response = {statusCode:1, additionalInfo:'Invalid parameters or missing parameters'};
+    res.json(response);
+  }
+  else{
   rest.post(url_base + '/pago', {
     data: { cuenta_cargo : charge_account,
             session_set: payload.session_set },
@@ -61,4 +78,5 @@ exports.payment =  function(req, res){
         res.json(response);
       }
     })
+  }
 };
