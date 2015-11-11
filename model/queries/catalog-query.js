b35a6b937192c8abcffc70df28e214f0041e2f0d
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2015-11-11 13:23:38 -0600

    Add mongodb script for dox information .

--- model/queries/catalog-query.js
+++ model/queries/catalog-query.js
@@ -2,7 +2,7 @@ var DoxInfo = require('../doxinfo');
 var config = require('../../config.js');
 
 exports.getDoxInfo = function (callback) {
-    var query = DoxInfo.find();
+    var query = DoxInfo.find({},'id description amount');
     query.sort({id : 1});
     query.exec(function(err, info) {
        if (err)
