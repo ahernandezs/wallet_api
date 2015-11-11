fcb9adf1f4d592a450169b2cc8c12deee1e4c738
Josue Guillermo Araujo Mayorga <nemux.1@gmail.com>
2015-11-10 19:25:44 -0600

    Return total unread messages in balance response.

--- routes/wallet.js
+++ routes/wallet.js
@@ -90,7 +90,8 @@ exports.balance = function(request, callback) {
 exports.getBalance = function(req, res) {
   console.log('execute GET method balance');
   console.log( req.headers['x-auth-token'] );
-  balance.balanceFlow(req.headers['x-auth-token'], function(err, result) {
+  console.log( req.headers['x-phoneid'] );
+  balance.balanceFlow(req.headers['x-auth-token'], req.headers['x-phoneid'], function(err, result) {
     if(result.statusCode === 0){
       res.setHeader('X-AUTH-TOKEN', result.sessionid);
       delete result.sessionid;
