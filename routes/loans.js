ba3508c60ee7c251cbb5abe7f2403e6275b4bcb1
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2015-11-30 15:26:41 -0600

    Add validation for skip rules in loan using blacklist .

--- routes/loans.js
+++ routes/loans.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var async = require('async');
 var loan = require('./flows/loan-flow');
 var loanQuery = require('../model/queries/loan-query');
+var userblackList = require('../model/queries/blacklist-query');
 var cashCreditService = require('../services/cashCredit-Service');
 var urbanService = require('../services/notification-service');
 var transaction = require('../model/transacction');
@@ -67,63 +68,87 @@ exports.getDecision = function(req,res) {
 }
 
 exports.loanConfirm = function(req,res){
-    logger.info('POST METHOD LOAN CONFIRM');
-    var payload = req.body;
-    payload.phoneID = req.headers['x-phoneid'];
-    payload.sessionid  = req.headers['x-auth-token'];
-    var amountLoan = payload.amount;
+  logger.info('POST METHOD LOAN CONFIRM');
+  var payload = req.body;
+  payload.phoneID = req.headers['x-phoneid'];
+  payload.sessionid  = req.headers['x-auth-token'];
+  var amountLoan = payload.amount;
 
-    if (!payload.phoneID && !payload.countryCode) {
+  if (!payload.phoneID && !payload.countryCode) {
         //res.status(400).send({message: 'The request JSON was invalid or cannot be served. '});
         res.send({'statusCode' : 1, additionalInfo: {'message': 'INVALID JSON'}});
         return;
-    }
+      }
 
-    transaction.getLastTransaction(payload.phoneID, config.transaction.operation.LOAN, function(err,transaction) {
-        if (err) {
-            console.log('{statusCode: 4, additionalInfo: {message: \'UNAVAILABLE DATABASE SERVICE\'}}');
-            res.send({statusCode: 4, additionalInfo: {message: 'UNAVAILABLE DATABASE SERVICE'}});
-            return;
-        }
+      async.waterfall([
+        function(callback){
+          userblackList.findUserByPhoneID(payload.phoneID,function(err,user){
+            if(user){
+              cashCreditService.requestLoan(payload, function(err,result){
+                console.log('Invoke create Loan Flow without restrictions');
+                var bodyContent = { phoneID : req.headers['x-phoneid'] , amount : amountLoan , sessionID :  req.headers['x-auth-token'] };
+                var payload = { body : bodyContent } ;
+                console.log('Invoke create Loan Flow 2');
+                console.log(payload);
+                loan.createLoanFlow(payload, function(err, resultLoan) {
+                  resultLoan.additionalInfo.doxEarned  = result.doxEarned;
+                  res.json(resultLoan);
+                });
+              });
+            }
+            else{
+              callback(null);
+            }
+          });
+        },
+        function(callback){
+          transaction.getLastTransaction(payload.phoneID, config.transaction.operation.LOAN, function(err,transaction) {
+            if (err) {
+              console.log('{statusCode: 4, additionalInfo: {message: \'UNAVAILABLE DATABASE SERVICE\'}}');
+              res.send({statusCode: 4, additionalInfo: {message: 'UNAVAILABLE DATABASE SERVICE'}});
+              return;
+            }
 
-        if(!transaction){
-            cashCreditService.requestLoan(payload, function(err,result){
+            if(!transaction){
+              cashCreditService.requestLoan(payload, function(err,result){
                 if(err) {
-                    res.send({statusCode: 12, additionalInfo: {message: 'UNAVAILABLE CASHCREDIT SERVICE'}});
-                    return;
+                  res.send({statusCode: 12, additionalInfo: {message: 'UNAVAILABLE CASHCREDIT SERVICE'}});
+                  return;
                 } else {
-                    console.log('Invoke create Loan Flow');
-                    var bodyContent = { phoneID : req.headers['x-phoneid'] , amount : amountLoan , sessionID :  req.headers['x-auth-token'] };
-                    var payload = { body : bodyContent } ;
-                    console.log('Invoke create Loan Flow 2');
-                    console.log(payload);
-                    loan.createLoanFlow(payload, function(err, resultLoan) {
-                        resultLoan.additionalInfo.doxEarned  = result.doxEarned;
-                        res.json(resultLoan);
-                    });
+                  console.log('Invoke create Loan Flow');
+                  var bodyContent = { phoneID : req.headers['x-phoneid'] , amount : amountLoan , sessionID :  req.headers['x-auth-token'] };
+                  var payload = { body : bodyContent } ;
+                  console.log('Invoke create Loan Flow 2');
+                  console.log(payload);
+                  loan.createLoanFlow(payload, function(err, resultLoan) {
+                    resultLoan.additionalInfo.doxEarned  = result.doxEarned;
+                    res.json(resultLoan);
+                  });
                 }
-            });
-        } else if ( ((new Date() - transaction.fecha)/1000) > 3600 ){
-            cashCreditService.requestLoan(payload, function(err,result){
+              });
+            } else if ( ((new Date() - transaction.fecha)/1000) > 3600 ){
+              cashCreditService.requestLoan(payload, function(err,result){
                 if(err) {
-                    res.send({statusCode: 12, additionalInfo: {message: 'UNAVAILABLE CASHCREDIT SERVICE'}});
-                    return;
+                  res.send({statusCode: 12, additionalInfo: {message: 'UNAVAILABLE CASHCREDIT SERVICE'}});
+                  return;
                 } else {
-                    console.log('Invoke create Loan Flow');
-                    var bodyContent = { phoneID : req.headers['x-phoneid'] , amount : amountLoan , sessionID :  req.headers['x-auth-token'] };
-                    var payload = { body : bodyContent } ;
-                    console.log('Invoke create Loan Flow 2');
-                    console.log(payload);
-                    loan.createLoanFlow(payload, function(err, resultLoan) {
-                        resultLoan.additionalInfo.doxEarned  = result.doxEarned;
-                        res.json(resultLoan);
-                    });
+                  console.log('Invoke create Loan Flow');
+                  var bodyContent = { phoneID : req.headers['x-phoneid'] , amount : amountLoan , sessionID :  req.headers['x-auth-token'] };
+                  var payload = { body : bodyContent } ;
+                  console.log('Invoke create Loan Flow 2');
+                  console.log(payload);
+                  loan.createLoanFlow(payload, function(err, resultLoan) {
+                    resultLoan.additionalInfo.doxEarned  = result.doxEarned;
+                    res.json(resultLoan);
+                  });
                 }
-            });
-        } else {
-            console.log('{statusCode: 14, additionalInfo : { message : \'ONLY 1 LOAN PER HOUR\' }}');
-            res.send({statusCode: 14, additionalInfo : { message : 'ONLY 1 LOAN PER HOUR' }});
-            return;
-        }
-    });
+              });
+            } else {
+              console.log('{statusCode: 14, additionalInfo : { message : \'ONLY 1 LOAN PER HOUR\' }}');
+              res.send({statusCode: 14, additionalInfo : { message : 'Only 1 loan per hour' }});
+              return;
+            }
+          });
+        },
+        ]);
 }
